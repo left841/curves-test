41274389b82254801aa14650758de54fc9e320ee
Savkin Yuriy <catasd@yandex.ru>
2024-08-18 02:32:06 +0300

    implemented base classes for points and curves

--- headers/curve_test.h
+++ headers/curve_test.h
@@ -1,6 +1,8 @@
 #ifndef __CURVE_TEST_H__
 #define __CURVE_TEST_H__
 
+#include <iostream>
+
 #ifdef _WIN32
 #ifdef curves_EXPORTS
 #define DYN_POLICY __declspec(dllexport)
@@ -10,5 +12,138 @@
 #else
 #define DYN_POLICY
 #endif // _WIN32
+
+namespace crv
+{
+    class DYN_POLICY point_3d
+    {
+    private:
+        double x = 0.0, y = 0.0, z = 0.0;
+
+    public:
+        point_3d() = default;
+        point_3d(const point_3d& src) = default;
+        point_3d& operator=(const point_3d& src) = default;
+        ~point_3d() = default;
+        point_3d(double _x, double _y, double _z);
+
+        double get_x() const;
+        void set_x(double value);
+        double get_y() const;
+        void set_y(double value);
+        double get_z() const;
+        void set_z(double value);
+
+        point_3d operator+(const point_3d& other) const;
+    };
+
+    DYN_POLICY std::ostream& operator<<(std::ostream& out, const point_3d& point);
+
+    class DYN_POLICY vector_3d
+    {
+    private:
+        point_3d initial, terminal;
+
+    public:
+        vector_3d() = default;
+        vector_3d(const vector_3d& src) = default;
+        vector_3d& operator=(const vector_3d& src) = default;
+        ~vector_3d() = default;
+        vector_3d(const point_3d& _initial, const point_3d& _terminal);
+
+        const point_3d& get_initial() const;
+        void set_initial(const point_3d& value);
+        const point_3d& get_terminal() const;
+        void set_terminal(const point_3d& value);
+
+        double magnitude() const;
+    };
+
+    DYN_POLICY std::ostream& operator<<(std::ostream& out, const vector_3d& vect);
+
+    class DYN_POLICY curve
+    {
+    public:
+        virtual point_3d get_point(double t) const = 0;
+        virtual point_3d get_first_derivative(double t) const = 0;
+        virtual vector_3d get_derivative_vector(double t) const = 0;
+    };
+
+    class DYN_POLICY circle: public curve
+    {
+    private:
+        point_3d centre;
+        double radius = 0.0;
+
+    public:
+        circle() = default;
+        circle(const circle& src) = default;
+        circle& operator=(const circle& src) = default;
+        ~circle() = default;
+        circle(double _radius);
+        circle(const point_3d& _centre, double _radius);
+
+        point_3d get_point(double t) const override;
+        point_3d get_first_derivative(double t) const override;
+        vector_3d get_derivative_vector(double t) const override;
+
+        const point_3d& get_centre() const;
+        void set_centre(const point_3d& value);
+        double get_radius() const;
+        void set_radius(double value);
+    };
+
+    class DYN_POLICY ellipse: public curve
+    {
+    private:
+        point_3d centre;
+        double radius_x = 0.0, radius_y = 0.0;
+
+    public:
+        ellipse() = default;
+        ellipse(const ellipse& src) = default;
+        ellipse& operator=(const ellipse& src) = default;
+        ~ellipse() = default;
+        ellipse(double _radius_x, double _radius_y);
+        ellipse(const point_3d& _centre, double _radius_x, double _radius_y);
+
+        point_3d get_point(double t) const override;
+        point_3d get_first_derivative(double t) const override;
+        vector_3d get_derivative_vector(double t) const override;
+
+        const point_3d& get_centre() const;
+        void set_centre(const point_3d& value);
+        double get_radius_x() const;
+        void set_radius_x(double value);
+        double get_radius_y() const;
+        void set_radius_y(double value);
+    };
+
+    class DYN_POLICY helix: public curve
+    {
+    private:
+        point_3d centre;
+        double radius = 0.0, step = 0.0;
+
+    public:
+        helix() = default;
+        helix(const helix& src) = default;
+        helix& operator=(const helix& src) = default;
+        ~helix() = default;
+        helix(double _radius, double _step);
+        helix(const point_3d& _centre, double _radius, double _step);
+
+        point_3d get_point(double t) const override;
+        point_3d get_first_derivative(double t) const override;
+        vector_3d get_derivative_vector(double t) const override;
+
+        const point_3d& get_centre() const;
+        void set_centre(const point_3d& value);
+        double get_radius() const;
+        void set_radius(double value);
+        double get_step() const;
+        void set_step(double value);
+    };
+}
 
 #endif // __CURVE_TEST_H__
--- sources/curve_test.cpp
+++ sources/curve_test.cpp
@@ -1,1 +1,242 @@
+#include <cmath>
 #include "curve_test.h"
+
+namespace crv
+{
+    constexpr const double pi = 3.14159265358979323846;
+
+    point_3d::point_3d(double _x, double _y, double _z) : x(_x), y(_y), z(_z)
+    {
+    }
+
+    double point_3d::get_x() const
+    {
+        return x;
+    }
+
+    void point_3d::set_x(double value)
+    {
+        x = value;
+    }
+
+    double point_3d::get_y() const
+    {
+        return y;
+    }
+
+    void point_3d::set_y(double value)
+    {
+        y = value;
+    }
+
+    double point_3d::get_z() const
+    {
+        return z;
+    }
+
+    void point_3d::set_z(double value)
+    {
+        z = value;
+    }
+
+    point_3d point_3d::operator+(const point_3d& other) const
+    {
+        return point_3d{ x + other.x, y + other.y, z + other.z };
+    }
+
+    std::ostream& operator<<(std::ostream& out, const point_3d& point)
+    {
+        return out << '{' << point.get_x() << ',' << point.get_y() << ',' << point.get_z() << '}';
+    }
+
+    vector_3d::vector_3d(const point_3d& _initial, const point_3d& _terminal) : initial(_initial), terminal(_terminal)
+    {
+    }
+
+    const point_3d& vector_3d::get_initial() const
+    {
+        return initial;
+    }
+
+    void vector_3d::set_initial(const point_3d& value)
+    {
+        initial = value;
+    }
+
+    const point_3d& vector_3d::get_terminal() const
+    {
+        return terminal;
+    }
+
+    void vector_3d::set_terminal(const point_3d& value)
+    {
+        terminal = value;
+    }
+
+    double vector_3d::magnitude() const
+    {
+        return std::sqrt((terminal.get_x() - initial.get_x()) * (terminal.get_x() - initial.get_x()) +
+            (terminal.get_y() - initial.get_y()) * (terminal.get_y() - initial.get_y()) +
+            (terminal.get_z() - initial.get_z()) * (terminal.get_z() - initial.get_z()));
+    }
+
+    std::ostream& operator<<(std::ostream& out, const vector_3d& vect)
+    {
+        return out << '{' << vect.get_initial() << ',' << vect.get_terminal() << '}';
+    }
+
+    circle::circle(double _radius): curve(), centre({ 0.0, 0.0, 0.0 }), radius(_radius)
+    {
+    }
+
+    circle::circle(const point_3d& _centre, double _radius): curve(), centre(_centre), radius(_radius)
+    {
+    }
+
+    point_3d circle::get_point(double t) const
+    {
+        point_3d result(radius * std::cos(t), radius * std::sin(t), 0.0);
+        return result + centre;
+    }
+
+    point_3d circle::get_first_derivative(double t) const
+    {
+        return { -radius * std::sin(t), radius * std::cos(t), 0.0 };
+    }
+
+    vector_3d circle::get_derivative_vector(double t) const
+    {
+        point_3d init_point = get_point(t);
+        return vector_3d{ init_point, init_point + get_first_derivative(t) };
+    }
+
+    const point_3d& circle::get_centre() const
+    {
+        return centre;
+    }
+
+    void circle::set_centre(const point_3d& value)
+    {
+        centre = value;
+    }
+
+    double circle::get_radius() const
+    {
+        return radius;
+    }
+
+    void circle::set_radius(double value)
+    {
+        radius = value;
+    }
+
+    ellipse::ellipse(double _radius_x, double _radius_y): curve(), centre({ 0.0, 0.0, 0.0 }), radius_x(_radius_x), radius_y(_radius_y)
+    {
+    }
+
+    ellipse::ellipse(const point_3d& _centre, double _radius_x, double _radius_y): curve(), centre(_centre), radius_x(_radius_x), radius_y(_radius_y)
+    {
+    }
+
+    point_3d ellipse::get_point(double t) const
+    {
+        point_3d result(radius_x * std::cos(t), radius_y * std::sin(t), 0.0);
+        return result + centre;
+    }
+
+    point_3d ellipse::get_first_derivative(double t) const
+    {
+        return { -radius_x * std::sin(t), radius_y * std::cos(t), 0.0 };
+    }
+
+    vector_3d ellipse::get_derivative_vector(double t) const
+    {
+        point_3d init_point = get_point(t);
+        return vector_3d{ init_point, init_point + get_first_derivative(t) };
+    }
+
+    const point_3d& ellipse::get_centre() const
+    {
+        return centre;
+    }
+
+    void ellipse::set_centre(const point_3d& value)
+    {
+        centre = value;
+    }
+
+    double ellipse::get_radius_x() const
+    {
+        return radius_x;
+    }
+
+    void ellipse::set_radius_x(double value)
+    {
+        radius_x = value;
+    }
+
+    double ellipse::get_radius_y() const
+    {
+        return radius_y;
+    }
+
+    void ellipse::set_radius_y(double value)
+    {
+        radius_y = value;
+    }
+
+    helix::helix(double _radius, double _step): curve(), centre({ 0.0, 0.0, 0.0 }), radius(_radius), step(_step)
+    {
+    }
+
+    helix::helix(const point_3d& _centre, double _radius, double _step): curve(), centre(_centre), radius(_radius), step(_step)
+    {
+    }
+
+    point_3d helix::get_point(double t) const
+    {
+        point_3d result(radius * std::cos(t), radius * std::sin(t), step * t / 2 / pi);
+        return result + centre;
+    }
+
+    point_3d helix::get_first_derivative(double t) const
+    {
+        return { -radius * std::sin(t), radius * std::cos(t), step / 2 / pi };
+    }
+
+    vector_3d helix::get_derivative_vector(double t) const
+    {
+        point_3d init_point = get_point(t);
+        return vector_3d{ init_point, init_point + get_first_derivative(t) };
+    }
+
+    const point_3d& helix::get_centre() const
+    {
+        return centre;
+    }
+
+    void helix::set_centre(const point_3d& value)
+    {
+        centre = value;
+    }
+
+    double helix::get_radius() const
+    {
+        return radius;
+    }
+
+    void helix::set_radius(double value)
+    {
+        radius = value;
+    }
+
+    double helix::get_step() const
+    {
+        return step;
+    }
+
+    void helix::set_step(double value)
+    {
+        step = value;
+    }
+}
